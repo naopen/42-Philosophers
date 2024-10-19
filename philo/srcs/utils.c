0a892fcc1a8ac23223e9241df62dec7cbd7b61c0
naopen <naokizssm@gmail.com>
2024-10-19 22:23:21 +0900

    fix: replace custom ft_atoi with ft_strtol

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 22:52:42 by nkannan           #+#    #+#             */
-/*   Updated: 2024/10/19 20:46:26 by nkannan          ###   ########.fr       */
+/*   Updated: 2024/10/19 22:19:28 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,9 @@ int	ft_isdigit(int c)
 	return (0);
 }
 
-int	ft_atoi(char *str)
+int	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	result;
-
-	i = 0;
-	sign = 1;
-	result = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (str[i] >= 48 && str[i] <= 57)
-	{
-		result = result * 10 + (str[i] - 48);
-		i++;
-	}
-	return (result * sign);
+	return ((int)ft_strtol(str, NULL, 10));
 }
 
 void	ft_bzero(void *s, size_t n)
